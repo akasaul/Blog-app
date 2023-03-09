47848414ee876c7a806d65872bbef4e84720cfb7
Nikodimos Jemaneh <nikodimosjemaneh40@gmail.com>
2023-03-09 05:19:33 -0500

    frontend setup and navbar

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <>
+    <Nav /> 
+
+    <Routes>
+        <Route path='/' element={<Home />} />
+    </Routes>
+    </>
+  );
+}
+
+export default App;
